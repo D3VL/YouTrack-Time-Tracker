62bb84cc6b254c9da92da3d89343b014fa31c608
D3VL Jack <jack@d3vl.com>
2023-07-15 20:26:54 +0100

    updated build workflow

--- scripts/update-manifest.js
+++ scripts/update-manifest.js
@@ -1,16 +1,14 @@
 const fs = require("fs");
 const path = require("path");
 
-const package = path.join(__dirname, "../package.json");
 const manifest = path.join(__dirname, "../public/manifest.json");
 
-const packageJson = require(package);
 const manifestJson = require(manifest);
 
-manifestJson.version = packageJson.version;
+manifestJson.version = process.argv[2];
 
 const json = JSON.stringify(manifestJson, null, 4);
 
 fs.writeFileSync(manifest, json);
 
-console.log("Updated manifest.json with version from package.json");
+console.log("Updated manifest.json with version " + process.argv[2]);
